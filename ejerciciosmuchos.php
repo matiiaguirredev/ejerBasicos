8d78397ebf8d829397909b989c58d981c02ebb3f
matiidev <matiasagui93@gmail.com>
2024-10-31 13:45:23 +0100

    mas practica

--- ejerciciosmuchos.php
+++ ejerciciosmuchos.php
@@ -66,3 +66,120 @@
 // } while ($i <= 15);
 
 
+// factorial simple con for
+// $num = readline("Ingrese un número para saber su factorial: ");
+// $facto = 1;
+
+// if (!is_numeric($num) || $num < 0) { // Cambiado a || para manejar condiciones de entrada inválida
+//     echo "Por favor ingrese un número entero y positivo.\n";
+//     exit();
+// }
+
+// for ($i = $num; $i > 1; $i--) {
+//     echo $facto *= $i;
+// }
+
+// echo "El factorial de $num es: $facto\n";
+
+
+
+// esto es factorial en funcion
+// function factorial($num) { 
+
+//     while (!is_numeric($num) || $num <= 0) {
+//         echo "Por favor ingrese un número entero y positivo.\n";
+//         $num = readline("Ingrese un número para saber su factorial: ");
+//     }
+
+//     $facto = 1;
+//     for ($i = $num; $i > 1; $i--) {
+//         $facto *= $i;
+//     }
+
+//     return $facto;
+// }
+
+// $factorialResult = factorial($num); // Llamamos a la función
+// echo "El factorial de $num es: $factorialResult\n";
+
+
+// esta es otra funcion que nos devuelve los factoriales de los num que pedimos, para terminar "fin"
+// function factorial($num) {
+//     $facto = 1;
+//     for ($i = $num; $i > 1; $i--) {
+//         $facto *= $i;
+//     }
+//     return $facto;
+// }
+
+// while (true) {
+//     $input = readline("Ingrese un número para saber su factorial (o escriba 'fin' para terminar): ");
+
+//     // Verificar si se desea terminar el programa
+//     if (strtolower(trim($input)) === 'fin') {
+//         echo "Programa finalizado.\n";
+//         break;
+//     }
+
+//     // Verificar si el valor ingresado es un número positivo
+//     if (!is_numeric($input) || $input < 0) {
+//         echo "Por favor, ingrese un número entero y positivo.\n";
+//         continue;
+//     }
+
+//     // Calcular e imprimir el factorial
+//     $num = (int)$input;
+//     $factorialResult = factorial($num);
+//     echo "El factorial de $num es: $factorialResult\n";
+// }
+
+// programa de descuento en compras
+// $total = 0;
+// while (true) {
+
+//     $costo = readline("Ingrese el monto de su compra: ");
+//     if (strtolower(trim($costo)) === 'fin') {
+//         echo "Ha finalizado su compra. \n";
+//         break;
+//     }
+
+//     if (!is_numeric($costo) || $costo < 0) {
+//         echo "Por favor, ingrese un número entero y positivo.\n";
+//         continue;
+//     }
+
+//     $total += $costo;
+//     $descuento = $total * 25 / 100;
+//     $resulFinal = $total - $descuento;
+// }
+
+// echo "El total de su compra fue: $$total, con un descuento de $$descuento y su compra queda en: $$resulFinal";
+
+
+
+$sumaNotas = 0;
+$cantiNotas = 0;
+
+while (true) {
+    $nota = readline("Ingrese una nota (o escriba 'fin' para finalizar): ");
+    
+    if (strtolower(trim($nota)) == "fin") {
+        echo "Ha finalizado la suma de sus notas.\n";
+        break;
+    }
+
+    if (!is_numeric($nota) || $nota <= 0) {
+        echo "Por favor, ingrese un número entero y positivo.\n";
+        continue;
+    }
+
+    $sumaNotas += $nota;
+    $cantiNotas++; 
+}
+
+if ($cantiNotas > 0) {
+    $promedio = $sumaNotas / $cantiNotas;
+    echo "La suma de sus notas es: $sumaNotas, ha ingresado $cantiNotas notas, y el promedio es: $promedio\n";
+} else {
+    echo "No se ingresaron notas válidas.\n";
+}
--- prueba.php
+++ prueba.php
@@ -1,4 +1,4 @@
-<?php
+<?php /*
 
 $altura = 25;
 
@@ -21,4 +21,53 @@ for ($i=$altura; $i >= 0 ; $i--) {
 
 echo '</div';
 
-?>
+ */ ?>
+
+<?php
+
+// factorial
+
+// $num = readline("Ingrese un número para saber su factorial: ");
+
+// function factorial($num) { 
+//     while (!is_numeric($num) || $num <= 0) {
+//         echo "Por favor ingrese un número entero y positivo.\n";
+//         $num = readline("Ingrese un número para saber su factorial: ");
+//     }
+
+//     $facto = 1;
+//     for ($i = $num; $i > 1; $i--) {
+//         $facto *= $i;
+//     }
+
+//     return $facto;
+// }
+
+// // Llamar a la función y mostrar el resultado
+// $factorialResult = factorial($num);
+// echo "El factorial de $num es: $factorialResult\n";
+
+
+
+// otra de facto rapida para prueba
+// $facto = 1;
+// $num = readline("Ingrese un número: ");
+// for ($i=$num; $i >1 ; $i--) { 
+//     $resul = $facto *= $i;
+// }
+
+// echo "El factorial de $num es: $resul\n";
+
+
+// par impar
+// $num = 7;
+
+// if ($num % 2 == 0) {
+//     echo "El número $num es par.\n";
+// }else {
+//     echo "El número $num es impar.\n";
+// }
+
+// numeros primos
+
+
